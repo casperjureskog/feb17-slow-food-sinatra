de6bd52f76bdb25f018225c431c1be6b0edb6cf7
johan <johan.schantz@telia.com>
2017-02-23 15:58:57 +0100

    removing restuarant files

--- spec/restaurant_spec.rb
+++
@@ -1,8 +0,0 @@
-require './lib/models/restaurant'
-
-describe Restaurant do
-   it { is_expected.to have_property :id }
-   it { is_expected.to have_property :name }
-
-   it { is_expected.to belong_to :user }
-end
